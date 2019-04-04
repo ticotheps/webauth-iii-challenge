b2f86ce0921671340845d986af30aa27f4737301
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-04 16:55:44 -0400

    Returns 'restricted' as a middleware function that is passed to the GET request handler of the users-router.js route

--- users/users-router.js
+++ users/users-router.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 
 const Users = require("./users-model.js");
-// const restricted = require("../auth/restricted-middleware.js");
+const restricted = require("../auth/restricted-middleware.js");
 
 // allows access to the database if the provided token is valid
-router.get("/", (req, res) => {
+router.get("/", restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
